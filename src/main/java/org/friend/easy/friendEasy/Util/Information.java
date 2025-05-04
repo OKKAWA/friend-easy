3be4314c4a686c70e17a86651cf996b2966b40dd
OKKAWA <songyi123123@outlook.com>
2025-05-04 10:19:26 +0800

    update

--- src/main/java/org/friend/easy/friendEasy/Util/Information.java
+++ src/main/java/org/friend/easy/friendEasy/Util/Information.java
@@ -1,5 +1,8 @@
 package org.friend.easy.friendEasy.Util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public interface Information {
     String VERSION = "1.1";
 
@@ -11,8 +14,14 @@ public interface Information {
     String GITHUBREPONAME = "friend-easy";
     String GITHUBUPDATEURL = null;
 
-    String EASTEREGG0 = "你发现了彩蛋，你是尝试要升级他们吗？如果你要这么做，我很欢迎并鼓励！";
-    String EASTEREGG1 = "孩子，你还年轻，写代码梦该醒醒了！";
+
+    List<String> GITHUBREPONAMES = List.of(
+            "你发现了彩蛋，你是尝试要升级他们吗？如果你要这么做，我很欢迎并鼓励！",
+            "孩子，你还年轻，写代码梦该醒醒了！",
+            "Welcome to Friend Easy",
+            "It's our world!",
+            "You're my dear friend！",
+            "OKKAWA 是我的github名称， AWA是我的本性");
 
 }
 
